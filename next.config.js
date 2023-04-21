6659fe0a2653ce4d5b6911286d7b3ca742c32057
Fabian Rubio <44301864+fabianrubio@users.noreply.github.com>
2023-04-21 17:50:56 -0600

    add message function

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-const withTM = require('next-transpile-modules')([
-  '@mui/material',
-  '@mui/system',
-]);
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  ...withTM,
 };
